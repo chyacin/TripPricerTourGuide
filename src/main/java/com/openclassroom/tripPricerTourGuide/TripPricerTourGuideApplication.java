e698941e1f61317f3ff0faa4e1d8e99624303577
chyacin <calvinhyacinth@gmail.com>
2021-02-23 12:47:02 +0100

    Created the Trip Pricer application

---
+++ src/main/java/com/openclassroom/tripPricerTourGuide/TripPricerTourGuideApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.tripPricerTourGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPricerTourGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPricerTourGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassroom/tripPricerTourGuide/TripPricerTourGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroom.tripPricerTourGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPricerTourGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
